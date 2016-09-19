6fbc85ceda457ce39057ff5e0e60569dd46bacb1
onvno <leewei2020@126.com>
2016-09-19 09:20:52 +0800

    add:增加checkout还原命令

---
+++ command/check.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const envPath = process.cwd();
+const execSync = require('child_process').execSync;
+const chalk = require('chalk');
+const fse = require('fs-extra');
+const path = require('path');
+
+// 仓库名-用于克隆或者更新仓库
+const frameDir = [
+	// 'sparrow',
+	'neoui',
+	// 'kero',
+	'kero-adapter',
+	'neoui-grid',
+	'neoui-tree',
+	'neoui-polyfill'
+];
+
+const dirs = fs.readdirSync(envPath); // 输出当前目录下的目录名
+
+module.exports = () => {
+	frameDir.forEach( function(element, index) {
+		var resPath = path.resolve(envPath,element);
+		const cssCMD = `cd ${resPath} && git checkout . && cd ..`;
+		execSync(cssCMD);
+	});
+
+};
